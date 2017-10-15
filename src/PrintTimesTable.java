1a55fdb4af16f7dd04584aae9613fe37f932f95c
Jordanjcp2332 <31933727+Jordanjcp2332@users.noreply.github.com>
2017-10-14 22:37:12 -0400

    positive works

--- src/PrintTimesTable.java
+++ src/PrintTimesTable.java
@@ -12,20 +12,64 @@ public class PrintTimesTable {
 		System.out.println("Lowest Number: ");
 		low=in.nextInt();
 		
-		int tab=(hi-low); 
-		int nump;
-		
-		
-		for (int x=low;x<=hi;x++) {
-			System.out.print("\t"+x+"\t");	
+		if(low<hi) {
+			int con=(hi-low)+1; 
+			int vert=low;
+			int vert2=low;
+			int hori=low; 
 			
-		}  
+			 System.out.print("\t\t");
+				for (int x=low;x<=hi;x++) {
+					System.out.print("\t"+x+"\t");	
+					
+				}  
+				while (con>0) {
+					 int con2=(hi-low)+1;
+					 System.out.print("\n\n\t"+vert2+"\t");
+					while (con2>0) {
+						
+						
+						System.out.print("\t"+vert*hori+"\t");
+						hori++;
+						con2--;
+						}
+					vert2++;
+					vert++;
+					hori=low;
+					con--;
+				}
+				
+		} else {
+			
+			int con=(low-hi)+1;
+			int vert=hi;
+			int vert2=hi;
+			int hori=hi;
+			
+			 System.out.print("\t\t");
+				for (int x=hi;x<=low;x++) {
+					System.out.print("\t"+x+"\t");	
+					
+				}  
+				while (con>0) {
+					 int con2=(low-hi)+1;
+					 System.out.print("\n\n\t"+vert2+"\t");
+					while (con2>0) {
+						
+						
+						System.out.print("\t"+vert*hori+"\t");
+						hori++;
+						con2--;
+						}
+					vert2++;
+					vert++;
+					hori=hi;
+					con--;
+				}
+				
+		}
 		
-		
-		
-		
-		
-
+	
 	}
 
 }
